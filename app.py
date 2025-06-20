bb5d352c40f1442499984e2019eba23b86d444e0
Shashank <144894099+git-shashank-hp@users.noreply.github.com>
2025-06-20 09:42:34 +0000

    new title

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def main():
         unsafe_allow_html=True,
     )
     default_text = (
-        "Welcome to the William Refrigeration Audio Experience. "
+        "Welcome to the Stremlit Audio Experience. "
         "Developed by Shashank to assist the creative design team in voice-over editing."
     )
     text = st.text_area(
